ca2dbdace9419e28e7d5aa62c4d564d77f1da4a0
gdedob <147407584+gdedob@users.noreply.github.com>
2023-12-06 10:01:02 +0100

    Update functions.php
    Optimisation de l'intégration des styles et script
    Ajout shortcode formulaire inscription
    Ajout tags programmes

--- medictate/functions.php
+++ medictate/functions.php
@@ -6,22 +6,20 @@ add_theme_support('menus');
 
 register_nav_menu('header', 'Entête du menu');
 
-// Charge les styles
+// Charge les styles et les scripts
 function wp_assets_loader() {
-    wp_enqueue_style ('bootstrap', 
-    'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css');
 
-    wp_enqueue_style('style_app',
-    get_template_directory_uri(). '/medictate/assets/css/app.css', 'bootstrap');
-wp_enqueue_script('bootstrap-script', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js',
-false,
-// version,
-true);
+  wp_enqueue_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css');
+  wp_enqueue_style('style', get_template_directory_uri() .'/assets/css/app.css', ['bootstrap'], true);
+
+  wp_enqueue_script('jquery');
+  wp_enqueue_script('bootstrap-bundle', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js', false, '1.0.0', true);
+
 }
-add_action('wp_enqueue_scripts', 'wp_assets_loader');
+add_action('wp_enqueue_scripts','wp_assets_loader');
 
 
-//
+//header
 function montheme_menu_class($classes) {
     // customizer classe des items (`li`)
     $classes[] = 'nav-item';
@@ -53,8 +51,26 @@ function create_posttypes() {
     }
 add_action('init', 'create_posttypes');
 
-// champs custom durée
+//Tag programmes
+function progs_tags_taxonomy() {
+    $labels = array(
+        'name' => 'Tags', // Le nom affiché de la taxonomie
+        'singular_name' => 'Tag', // Le nom singulier
+    );
 
+    $args = array(
+        'labels' => $labels,
+        'public' => true,
+        'hierarchical' => false,
+    );
+
+    register_taxonomy('progs_tags', 'progs', $args);
+}
+add_action('init', 'progs_tags_taxonomy');
+
+
+
+// champs custom durée
 // Ajouter un méta champ pour la durée du programme
 function ajouter_duree() {
     add_meta_box(
@@ -86,6 +102,66 @@ function sauvegarder_duree($post_id) {
     }
 }
 add_action('save_post', 'sauvegarder_duree');
+
+//short code inscription
+function form_inscription_shortcode() {
+    ob_start();
+    ?>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <h2 class="text-center">Créer un compte</h2>
+                <form id="inscription-form">
+                    <div class="form-group">
+                        <label for="nom">Nom</label>
+                        <input type="text" class="form-control" id="nom" name="nom" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" class="form-control" id="email" name="email" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="motdepasse">Mot de passe</label>
+                        <input type="password" class="form-control" id="motdepasse" name="motdepasse" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Créer un compte</button>
+                </form>
+                <div id="inscription-result"></div>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        // Envoi l'inscription à la DB
+        jQuery(document).ready(function($) {
+            $('#inscription-form').submit(function(e) {
+                e.preventDefault();
+                var formData = $(this).serialize();
+                $.ajax({
+                    type: 'POST',
+                    dataType: 'json',
+                    url: '<?php echo admin_url('admin-ajax.php'); ?>',
+                    data: formData + '&action=inscription_process',
+                    success: function(response) {
+                        $('#inscription-result').html(response.message);
+                        if (response.success) {
+                            // Pop-up de remerciement (tu peux utiliser une librairie comme Bootstrap pour cela).
+                            alert('Merci de votre inscription');
+                            // Rediriger vers la page d'accueil après 5 secondes (5000 millisecondes).
+                            setTimeout(function() {
+                                window.location.href = '<?php echo home_url(); ?>';
+                            }, 5000);
+                        }
+                    }
+                });
+            });
+        });
+    </script>
+    <?php
+    return ob_get_clean();
+}
+add_shortcode('form_inscription', 'form_inscription_shortcode');
+
 
 //Création d'utilisateur
 function inscription_process() {
@@ -128,4 +204,6 @@ function inscription_process() {
 
 add_action('wp_ajax_inscription_process', 'inscription_process');
 add_action('wp_ajax_nopriv_inscription_process', 'inscription_process');
+
+
 
