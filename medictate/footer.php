45666789affcaadeec4ce03c50416a84aa55ee48
gdedob <147407584+gdedob@users.noreply.github.com>
2023-12-12 13:56:29 +0100

    Début de footer
    Pas encore lié au css

--- medictate/footer.php
+++ medictate/footer.php
@@ -1,1 +1,40 @@
-footer
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <!-- Colonne 1 -->
+                <div class="footer-column">
+                    <!-- Logo avec slogan -->
+                    <img src="chemin/vers/votre/logo.png" alt="Logo">
+                    <p>Votre slogan ici</p>
+                    
+                    <!-- Lien vers les mentions légales -->
+                    <a href="lien-vers-mentions-legales">Mentions légales</a>
+                    
+                    <!-- Adresse et adresse e-mail -->
+                    <p>Adresse de la société</p>
+                    <p>Email : contact@societe.com</p>
+                </div>
+            </div>
+            
+            <div class="col-md-4">
+                <!-- Colonne 2 -->
+                <div class="footer-column">
+                    <!-- Liens vers les pages Wedictate et Medictate -->
+                    <a href="lien-vers-wedictate">Wedictate</a>
+                    <a href="lien-vers-medictate">Medictate</a>
+                </div>
+            </div>
+            
+            <div class="col-md-4">
+                <!-- Colonne 3 -->
+                <div class="footer-column">
+                    <!-- Liens vers Contact, À propos et Contact -->
+                    <a href="lien-vers-contact">Contact</a>
+                    <a href="lien-vers-a-propos">À propos</a>
+                    <a href="lien-vers-reseaux-sociaux">Réseaux sociaux</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
