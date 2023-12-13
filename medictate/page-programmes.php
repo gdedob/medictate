47731649cbd2370c8850b692ec2a468642fdf129
gdedob <147407584+gdedob@users.noreply.github.com>
2023-12-13 15:01:52 +0100

    Durée et catégorie sélectionnable

--- medictate/page-programmes.php
+++ medictate/page-programmes.php
@@ -1,54 +1,105 @@
-<?php /* Template Name: Liste Programmes */ get_header(); ?>
+<?php /*Template Name: Programmes */ get_header(); ?>
 
-<main class="padding-container">
-
-<!-- Récupérer les tags des programmes de méditation -->
 <?php
- $tags = get_terms('progs_tags',);
-?>
+// Initialisation des tableaux pour les catégories et durées
+$categories = array();
+$durations = array();
 
+// Requête pour récupérer tous les articles du type 'progs'
+$progs_query = new WP_Query(array(
+    'post_type' => 'progs', // Spécifie le type de post 'progs'
+    'posts_per_page' => -1, // Récupère tous les articles
+));
 
-<!-- Afficher le formulaire de filtrage par tag -->
-<section>
-<form method="get">
-    <select name="tag_filter">
-        <option value="">Tous les Tags</option>
-        <?php // foreach ($tags as $tag) : ?>
-            <option value="<?php // echo esc_attr($tag->slug); ?>" <?php // selected($_GET['tag_filter'], $tag->slug); ?>><?php // echo esc_html($tag->name); ?></option>
-        <?php // endforeach; ?>
-    </select>
-    <input type="submit" value="Filtrer">
-</form>
+// Vérifie si des articles sont trouvés
+if ($progs_query->have_posts()) {
+    while ($progs_query->have_posts()) {
+        $progs_query->the_post();
+
+        // Récupère l'ID de l'article 'progs' actuel
+        $programme_id = get_the_ID();
+
+        // Récupère les valeurs pour 'categorie_programme' et 'duree_programme' pour chaque article
+        $programme_categories = get_post_meta($programme_id, 'categorie_programme', true);
+        $programme_durations = get_post_meta($programme_id, 'duree_programme', true);
+
+        // Ajoute les valeurs récupérées aux tableaux s'ils existent et ne sont pas déjà présentes
+        if (!empty($programme_categories) && !in_array($programme_categories, $categories)) {
+            $categories[] = $programme_categories;
+        }
+
+        if (!empty($programme_durations) && !in_array($programme_durations, $durations)) {
+            $durations[] = $programme_durations;
+        }
+    }
+} ?>
+
+<section class="container mt-5">
+    <form method="get" class="row g-3">
+        <!-- Sélecteur pour filtrer par catégorie -->
+        <div class="col-md-6">
+            <select name="category_filter" class="form-select">
+                <option value="">Toutes les Catégories</option>
+                <?php foreach (array_unique($categories) as $category) : ?>
+                    <option value="<?php echo esc_attr($category); ?>"><?php echo esc_html($category); ?></option>
+                <?php endforeach; ?>
+            </select>
+        </div>
+
+        <!-- Sélecteur pour filtrer par durée -->
+        <div class="col-md-6">
+            <select name="duration_filter" class="form-select">
+                <option value="">Toutes les Durées</option>
+                <?php foreach (array_unique($durations) as $duration) : ?>
+                    <option value="<?php echo esc_attr($duration); ?>"><?php echo esc_html($duration); ?> minutes</option>
+                <?php endforeach; ?>
+            </select>
+        </div>
+
+        <div>
+            <input type="submit" value="Filtrer" class="btn btn-primary">
+        </div>
+    </form>
 </section>
 
 <?php
-$args = isset($_GET['tag_filter']) && !empty($_GET['tag_filter']) ? [
-    'tax_query' => [
-        [
-            'taxonomy' => 'progs_tags',
-            'field' => 'slug',
-            'terms' => $_GET['tag_filter'],
-        ],
-    ],
-] : [];
+$args = [
+    'post_type' => 'progs',
+    'posts_per_page' => -1,
+    'meta_query' => [], // Initialise le tableau pour les filtres de métadonnées
+];
+
+// Ajout des filtres s'ils sont sélectionnés
+if (isset($_GET['category_filter']) && $_GET['category_filter'] !== '') {
+    $args['meta_query'][] = [
+        'key' => 'categorie_programme',
+        'value' => $_GET['category_filter'],
+    ];
+}
+
+if (isset($_GET['duration_filter']) && $_GET['duration_filter'] !== '') {
+    $args['meta_query'][] = [
+        'key' => 'duree_programme',
+        'value' => $_GET['duration_filter'],
+    ];
+}
+
+$programmesList = new WP_Query($args);
 ?>
 
-<!-- affiche les articles -->
-    <section>
-        <div class="container">  
-            <?php 
-            $programmesList = new WP_Query([
-            'posts_per_page' => -1,
-            'post_type' => 'progs'
-            ]);
-
-      while ($programmesList->have_posts()): $programmesList->the_post();
-    ?>
-                <?php the_title(); ?>
-                <a href="<?php the_permalink(); ?>">Découvrir le programme</a>
-            <?php endwhile; ?>
-        </div>
-    </section>
-</main>
+<section class="container mt-5">
+    <div class="row">
+        <?php while ($programmesList->have_posts()) : $programmesList->the_post(); ?>
+            <div class="col-md-4 mb-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title"><?php the_title(); ?></h5>
+                        <a href="<?php the_permalink(); ?>" class="btn btn-primary">Découvrir le programme</a>
+                    </div>
+                </div>
+            </div>
+        <?php endwhile; ?>
+    </div>
+</section>
 
 <?php get_footer(); ?>
