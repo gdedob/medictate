a1938ddd1b8c5047ab3b13711d18fae7b85261d5
gdedob <147407584+gdedob@users.noreply.github.com>
2023-12-16 16:47:15 +0100

    "Playlists"

--- medictate/page-medictate.php
+++ medictate/page-medictate.php
@@ -14,61 +14,61 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "Tous les champs sont requis";
     } else {
         // Création des playlists en fonction des choix. Pas de base de données, donc créé manuellement
-            $playlists = array(
+                        $playlists = array(
                 'stress' => array(
                     'nature' => array(
                         '10' => array('7Vc4-FDGBxo', 'UOAwvnXf8cE', 'tIZpNrr0uX8'),
-                        '20' => array('videoID1', 'videoID2', 'videoID3'),
-                        '30' => array('videoID1', 'videoID2', 'videoID3')
+                        '20' => array('dEmVeTsQEqQ', 'KqivhqCjPNA', 'hxIlvfTDNuE'),
+                        '30' => array('J8i7v0MozjY', '1_1X9l-Igmw', 'DuBjnlGCr3c')
                     ),
-                    'animaux' => array(
-                        '10' => array('videoID1', 'videoID2', 'videoID3'),
-                        '20' => array('videoID1', 'videoID2', 'videoID3'),
-                        '30' => array('videoID1', 'videoID2', 'videoID3')
+                    'blancs' => array(
+                        '10' => array('DqewBvd-bAA', 'tjo40K3Lscg', '1lg4eHJIi-U'),
+                        '20' => array('tLtc25KKXlQ', 'UTrLuAKFJr0', 'N7MhSN5UyLY'),
+                        '30' => array('gRx8EWPBRdE', 'aJYya2PjJPo', 'mwF-N6QV320')
                     ),
                     'classique' => array(
-                        '10' => array('videoID1', 'videoID2', 'videoID3'),
-                        '20' => array('videoID1', 'videoID2', 'videoID3'),
-                        '30' => array('videoID1', 'videoID2', 'videoID3')
+                        '10' => array('DmMNqiMNUec', 'chbCyQ3Cj5Q', 'wT5wB8KgiJE'),
+                        '20' => array('jACIurNDuWA', 'dr5kIR4zhCs', 'SG9HdGZynWU'),
+                        '30' => array('xWpkKQH0xco', 'ZvZ95vAHfxM', 'mdSHRjp1mJY')
                     )
                 ),
                 'addiction' => array(
                     'nature' => array(
-                        '10' => array('videoID1', 'videoID2', 'videoID3'),
-                        '20' => array('videoID1', 'videoID2', 'videoID3'),
-                        '30' => array('videoID1', 'videoID2', 'videoID3')
+                        '10' => array('4hXYRXaJdtk', 'pI2iqkW6Fw8', '4DaZ0cBIxpg'),
+                        '20' => array('V-P3QhBIRHM', 'gRl0trDBrwg', 'j3fJjY0YZ4Y'),
+                        '30' => array('I63sF8pZfEM', 'm6xBRts6J_E', '3WXFZzFuty0')
                     ),
-                    'animaux' => array(
-                        '10' => array('videoID1', 'videoID2', 'videoID3'),
-                        '20' => array('videoID1', 'videoID2', 'videoID3'),
-                        '30' => array('videoID1', 'videoID2', 'videoID3')
+                    'blancs' => array(
+                        '10' => array('NZs-WK3DYpQ', 'LZYK-HqFTV4', 'LNuGsjAr9aU'),
+                        '20' => array('2r02Qkwl46g', 'OEZzZz7qHaI', 'KXpHt1FvxZE'),
+                        '30' => array('CpS5Ex1Wx-4', 'mwF-N6QV320', 'SniyjT0N5-0')
                     ),
                     'classique' => array(
-                        '10' => array('videoID1', 'videoID2', 'videoID3'),
-                        '20' => array('videoID1', 'videoID2', 'videoID3'),
-                        '30' => array('videoID1', 'videoID2', 'videoID3')
+                        '10' => array('Un3ZMgVBI7s', 'ObKJkYFMpGc', 'IiToAcBoUks'),
+                        '20' => array('ohGHvRp3WtM', 'yUgJb-xDWoU', 'NMo5iCsphxI'),
+                        '30' => array('FKi5AHvUeOQ', 'NiGItPUe-EI', 'ewBY6T9XqNU')
                     )
                 ),
                 'concentration' => array(
                     'nature' => array(
-                        '10' => array('videoID1', 'videoID2', 'videoID3'),
-                        '20' => array('videoID1', 'videoID2', 'videoID3'),
-                        '30' => array('videoID1', 'videoID2', 'videoID3')
+                        '10' => array('p7oVVBW_BHw', 'P1Mox-Hny7Y', 'wZH9QLcrtM8'),
+                        '20' => array('_wxQGO_Pw-w', 'AfbZM584OJM', 'O6bYEpgKARE'),
+                        '30' => array('qWoVaVXpiO4', 'GhqMwdP1fc8', 'YKxXs_FzgQo')
                     ),
-                    'animaux' => array(
-                        '10' => array('videoID1', 'videoID2', 'videoID3'),
-                        '20' => array('videoID1', 'videoID2', 'videoID3'),
-                        '30' => array('videoID1', 'videoID2', 'videoID3')
+                    'blancs' => array(
+                        '10' => array('gRRbHz6WvTE', 'YIg1TwCoPVk', 'i-aJA1eTlFk'),
+                        '20' => array('VfqQrVbNjXc', 'mIGUatZsjsU', '1XEoMTiK7PA'),
+                        '30' => array('RfkohDtz76Q', '1o65g173sh0', 'j98SzqfU3J4')
                     ),
                     'classique' => array(
-                        '10' => array('videoID1', 'videoID2', 'videoID3'),
-                        '20' => array('videoID1', 'videoID2', 'videoID3'),
-                        '30' => array('videoID1', 'videoID2', 'videoID3'),
+                        '10' => array('LXEeONm6hy8', 'VyidxkkniOU', '9j8JNX_pcYc'),
+                        '20' => array('0L-O4hc88xA', 'Kj6QFnWWbNE', 'eMmvZJ6NmDQ'),
+                        '30' => array('7IoF8Aomfrg', 'yfU6a-buJJQ', 'Cp6ElfcLB4Y'),
                     )
                 )
-             );
+             ); ?>
 
-       // Sélection de la playlist correspondant aux choix de l'utilisateur (cahcune entre crochet, car chacune correspond à un array différent)
+    <?php   // Sélection de la playlist correspondant aux choix de l'utilisateur (cahcune entre crochet, car chacune correspond à un array différent)
       $selectedPlaylist = $playlists[$theme][$audio][$duree];
 
     // Mélanger aléatoirement la playlist
