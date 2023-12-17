f5dec9c1a3badb61f60f0489d92a492562223b69
gdedob <147407584+gdedob@users.noreply.github.com>
2023-12-17 20:45:20 +0100

    Correction carousel

--- medictate/page-wedictate.php
+++ medictate/page-wedictate.php
@@ -23,7 +23,7 @@ get_header();
             // Vérifier chaque cas de la valeur du champ "visuel" et afficher le carrousel correspondant
             if ($selected_visuel === 'formes') {
                 ?>
-                <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel" data-bs-interval="1000">
+                <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
                     <div class="carousel-inner">
                         <div class="carousel-item active">
                             <img src="<?php echo get_template_directory_uri(); ?>/assets/img/forme1.jpg" class="d-block w-100" alt="...">
@@ -35,19 +35,11 @@ get_header();
                             <img src="<?php echo get_template_directory_uri(); ?>/assets/img/forme3.jpg" class="d-block w-100" alt="...">
                         </div>
                     </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
                 </div>
                 <?php
             } elseif ($selected_visuel === 'paysages') {
                 ?>
-                <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel" data-bs-interval="1000">
+                <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
                     <div class="carousel-inner">
                         <div class="carousel-item active">
                             <img src="<?php echo get_template_directory_uri(); ?>/assets/img/paysage1.jpg" class="d-block w-100" alt="...">
@@ -59,19 +51,11 @@ get_header();
                             <img src="<?php echo get_template_directory_uri(); ?>/assets/img/paysage3.jpg" class="d-block w-100" alt="...">
                         </div>
                     </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
                 </div>
                 <?php
             } elseif ($selected_visuel === 'zen') {
                 ?>
-                <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel" data-bs-interval="1000">
+                <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
                     <div class="carousel-inner">
                         <div class="carousel-item active">
                             <img src="<?php echo get_template_directory_uri(); ?>/assets/img/zen1.jpg" class="d-block w-100" alt="...">
@@ -83,14 +67,6 @@ get_header();
                             <img src="<?php echo get_template_directory_uri(); ?>/assets/img/zen3.jpg" class="d-block w-100" alt="...">
                         </div>
                     </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
                 </div>
                 <?php
             } else {
