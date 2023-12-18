a4d522a23deb76b409f3bb52478639038d5a2cd8
gdedob <147407584+gdedob@users.noreply.github.com>
2023-12-18 15:00:31 +0100

    Update and rename formulaire-ambiance.php to page-formambiance.php

--- medictate/page-formambiance.php
+++ medictate/page-formambiance.php
@@ -1,5 +1,6 @@
 <?php /*Template Name: Créateur d'ambiance*/ get_header(); ?>
 
+
 <form id="formambi" action="<?php echo esc_url(home_url('/medictate')); ?>" method="post">
     <div class="container mt-4">
         <!-- Thème -->
@@ -105,75 +106,73 @@
     <div class="alert alert-warning" id="error-message"></div>
     <div class="d-flex justify-content-center">
         <input type="image" src="<?php echo get_template_directory_uri(); ?>/assets/img/ico/nav/icomedictate.svg" 
-            alt="Valider" class="btn btn-primary submit-button" style="max-width: 25%; height: auto;">
+            alt="Valider" class="btn btn-primary submit-button" id="bouton-envoi" style="max-width: 25%; height: auto;">
     </div>
 </form>
 
-<script>
-    $(document).ready(function() {
-        // Attend que le document soit chargé pour exécuter le code jQuery
-        $('#formambi').submit(function(event) {
-            // Lors de la soumission du formulaire, exécute la fonction
-            const theme = $('input[name="theme"]:checked').val();
-            const duree = $('#duree').val();
-            const audio = $('input[name="audio"]:checked').val();
-            const visuel = $('input[name="visuel"]:checked').val();
 
-            const errorElement = $('#error-message');
 
-            // Vérifie si tous les champs requis sont remplis
-            if (!theme || !audio || !visuel) {
 
-                errorElement.text('Veuillez remplir tous les champs.');
-                event.preventDefault(); // Empêche l'envoi du formulaire
-            }
-        });
-    })
-</script>
 
 <script>
     $(document).ready(function() {
-        // Fonction pour vérifier si tous les champs requis sont remplis
-        function checkFormValidity() {
-            let isValid = true;
-
-            // Vérification pour chaque groupe de champs
-            const formGroups = [
-                'input[name="theme"]',
-                '#duree',
-                'input[name="audio"]',
-                'input[name="visuel"]'
-            ];
-
-            formGroups.forEach(function(selector) {
-                if ($(selector).filter(':checked').length === 0) {
-                    isValid = false;
-                    return false; // Sortir de la boucle si un champ est vide
-                }
-            });
-
-            return isValid;
-        }
-
         // Fonction pour afficher le bouton avec animation ou le message d'erreur
         function displayButtonOrError() {
-            if (checkFormValidity()) {
+            const errorElement = $('#error-message');
+            const isValid = $('input[name="theme"]:checked').length > 0 &&
+                            $('#duree').val() !== '' &&
+                            $('input[name="audio"]:checked').length > 0 &&
+                            $('input[name="visuel"]:checked').length > 0;
+
+            if (isValid) {
                 // Afficher le bouton avec une animation
-                $('.btn-primary').fadeIn().css('transform', 'rotate(360deg)');
+                $('.bouton-envoi').fadeIn().css('transform', 'rotate(360deg)');
+                errorElement.text(''); // Effacer le message d'erreur s'il y en avait un
             } else {
                 // Afficher le message d'erreur
-                alert('Tous les champs sont requis');
+                errorElement.text('Veuillez remplir tous les champs.');
             }
         }
 
         // Cacher le bouton au chargement de la page
-        $('.btn-primary').hide();
+        $('.bouton-envoi').hide();
 
         // Vérifier lorsque les champs changent et appeler la fonction appropriée
-        $('input, #duree').on('change', displayButtonOrError);
+        $('input, #duree').on('input', displayButtonOrError);
+
+        // Attendre la soumission du formulaire pour exécuter la vérification finale
+        $('#formambi').submit(function(event) {
+            const errorElement = $('#error-message');
+            const isValid = $('input[name="theme"]:checked').length > 0 &&
+                            $('#duree').val() !== '' &&
+                            $('input[name="audio"]:checked').length > 0 &&
+                            $('input[name="visuel"]:checked').length > 0;
+
+            if (!isValid) {
+                errorElement.text('Veuillez remplir tous les champs.');
+                event.preventDefault(); // Empêcher l'envoi du formulaire si des champs sont vides
+            }
+        });
     });
 </script>
 
+<script> //affichage de la durée à côté du curseur
+    // Récupération de l'élément range et de l'élément pour afficher la valeur
+    const range = document.getElementById('duree');
+    const valueIndicator = document.querySelector('.value-indicator');
+
+    // Écouteur d'événements pour détecter les changements sur le range
+    range.addEventListener('input', function() {
+        // Récupération de la valeur sélectionnée sur le range
+        const value = this.value;
+        // Calcul de la position pour placer l'indicateur de valeur
+        const position = (value / parseInt(this.max)) * 100;
+        // Affichage de la valeur sélectionnée dans l'élément span
+        valueIndicator.textContent = value;
+        // Placement de l'indicateur de valeur à côté du curseur
+        valueIndicator.style.left = `calc(${position}% + (${8 - position * 0.15}px))`;
+    });
+</script>
 
 
 <?php get_footer(); ?>
