501b01e27aa8727810bda45b2e190b57de8f4e66
gdedob <147407584+gdedob@users.noreply.github.com>
2023-12-18 14:25:44 +0100

    Formulaire responsive (wip) + script (wip)

--- medictate/formulaire-ambiance.php
+++ medictate/formulaire-ambiance.php
@@ -1,119 +1,135 @@
 <?php /*Template Name: Créateur d'ambiance*/ get_header(); ?>
 
+<form id="formambi" action="<?php echo esc_url(home_url('/medictate')); ?>" method="post">
+    <div class="container mt-4">
+        <!-- Thème -->
+        <div class="row mb-3 justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-body">
+                        <label for="theme" class="form-label">Sélectionner un thème :</label>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="theme" id="theme-stress" value="stress" required>
+                            <label class="form-check-label" for="theme-stress">
+                                Gestion du stress
+                            </label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="theme" id="theme-concentration" value="concentration" required>
+                            <label class="form-check-label" for="theme-concentration">
+                                Amélioration de la concentration
+                            </label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="theme" id="theme-addiction" value="addiction" required>
+                            <label class="form-check-label" for="theme-addiction">
+                                Gestion des addictions
+                            </label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-<form action="<?php echo esc_url(home_url('/medictate')); ?>" method="post">
-    <div class="form-group">
-        <label>Sélectionner un thème :</label><br>
-        <div class="form-check">
-            <input class="form-check-input" type="radio" name="theme" id="theme-stress" value="stress" required>
-            <label class="form-check-label" for="theme-stress">
-                Gestion du stress
-            </label>
+        <!-- Durée, Audio, Visuel dans des cartes -->
+        <div class="row row-cols-1 row-cols-md-2 justify-content-center">
+            <div class="col mb-3 col-md-4">
+                <div class="card d-inline-block">
+                    <div class="card-body">
+                        <label for="duree" class="form-label">Durée (en minutes) :</label>
+                        <div class="input-group">
+                            <input type="range" class="form-control-range" id="duree" name="duree" min="10" max="30" step="10" list="tickmarks" required>
+                            <datalist id="tickmarks">
+                                <option value="10">10</option>
+                                <option value="20">20</option>
+                                <option value="30">30</option>
+                            </datalist>
+                            <span class="value-indicator"></span>
+                            <span class="error-message" id="duree-error"></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col mb-3 col-md-4">
+                <div class="card d-inline-block">
+                    <div class="card-body">
+                        <label class="form-label">Sélectionner un type d'audio :</label>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="audio" id="audio-nature" value="nature" required>
+                            <label class="form-check-label" for="audio-nature">
+                                Sons de la nature
+                            </label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="audio" id="audio-classique" value="classique" required>
+                            <label class="form-check-label" for="audio-classique">
+                                Musique classique
+                            </label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="audio" id="audio-blancs" value="animaux" required>
+                            <label class="form-check-label" for="audio-blancs">
+                                Bruits blancs
+                            </label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col mb-3 col-md-4">
+                <div class="card d-inline-block">
+                    <div class="card-body">
+                        <label class="form-label">Sélectionner un visuel :</label>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="visuel" id="visuel-formes" value="formes_geometriques" required>
+                            <label class="form-check-label" for="visuel-formes">
+                                Formes géométriques et couleurs
+                            </label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="visuel" id="visuel-paysages" value="paysages" required>
+                            <label class="form-check-label" for="visuel-paysages">
+                                Paysages
+                            </label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="visuel" id="visuel-zen" value="zen" required>
+                            <label class="form-check-label" for="visuel-zen">
+                                Imagerie zen
+                            </label>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="form-check">
-            <input class="form-check-input" type="radio" name="theme" id="theme-concentration" value="concentration" required>
-            <label class="form-check-label" for="theme-concentration">
-                Amélioration de la concentration
-            </label>
-        </div>
-        <div class="form-check">
-            <input class="form-check-input" type="radio" name="theme" id="theme-addiction" value="addiction" required>
-            <label class="form-check-label" for="theme-addiction">
-                Gestion des addictions
-            </label>
-        </div>
-        <span class="error-message" id="theme-error"></span>
     </div>
-
-    <div class="form-group">
-        <label for="duree">Durée (en minutes) :</label>
-        <input type="range" class="form-control-range" id="duree" name="duree" min="10" max="30" step="10" list="tickmarks" required>
-        <datalist id="tickmarks">
-            <option value="10">10</option>
-            <option value="20">20</option>
-            <option value="30">30</option>
-        </datalist>
-        <span class="error-message" id="duree-error"></span>
+    <div class="alert alert-warning" id="error-message"></div>
+    <div class="d-flex justify-content-center">
+        <input type="image" src="<?php echo get_template_directory_uri(); ?>/assets/img/ico/nav/icomedictate.svg" 
+            alt="Valider" class="btn btn-primary submit-button" style="max-width: 25%; height: auto;">
     </div>
-
-    <div class="form-group">
-        <label>Sélectionner un type d'audio :</label><br>
-        <div class="form-check">
-            <input class="form-check-input" type="radio" name="audio" id="audio-nature" value="nature" required>
-            <label class="form-check-label" for="audio-nature">
-                Sons de la nature
-            </label>
-        </div>
-        <div class="form-check">
-            <input class="form-check-input" type="radio" name="audio" id="audio-classique" value="classique" required>
-            <label class="form-check-label" for="audio-classique">
-                Musique classique
-            </label>
-        </div>
-        <div class="form-check">
-            <input class="form-check-input" type="radio" name="audio" id="audio-animaux" value="animaux" required>
-            <label class="form-check-label" for="audio-animaux">
-                Bruits d'animaux
-            </label>
-        </div>
-    </div>
-
-    <div class="form-group">
-        <label>Sélectionner un visuel :</label><br>
-        <div class="form-check">
-            <input class="form-check-input" type="radio" name="visuel" id="visuel-formes" value="formes_geometriques" required>
-            <label class="form-check-label" for="visuel-formes">
-                Formes géométriques et couleurs
-            </label>
-        </div>
-        <div class="form-check">
-            <input class="form-check-input" type="radio" name="visuel" id="visuel-paysages" value="paysages" required>
-            <label class="form-check-label" for="visuel-paysages">
-                Paysages
-            </label>
-        </div>
-        <div class="form-check">
-            <input class="form-check-input" type="radio" name="visuel" id="visuel-zen" value="zen" required>
-            <label class="form-check-label" for="visuel-zen">
-                Imagerie zen
-            </label>
-        </div>
-        <span class="error-message" id="visuel-error"></span>
-    </div>
-
-
-    <input type="image" src="<?php echo get_template_directory_uri(); ?>/assets/img/ico/nav/icomedictate.svg" 
-    alt="Valider" class="btn btn-primary submit-button">
 </form>
-</form>
 
-<script> /*
+<script>
     $(document).ready(function() {
         // Attend que le document soit chargé pour exécuter le code jQuery
-        $('#myForm').submit(function(event) {
+        $('#formambi').submit(function(event) {
             // Lors de la soumission du formulaire, exécute la fonction
             const theme = $('input[name="theme"]:checked').val();
             const duree = $('#duree').val();
             const audio = $('input[name="audio"]:checked').val();
             const visuel = $('input[name="visuel"]:checked').val();
 
-            const errorElement = $('#form-error');
-            let isValid = true;
+            const errorElement = $('#error-message');
 
             // Vérifie si tous les champs requis sont remplis
-            if (!theme || !duree || !audio || !visuel) {
-                isValid = false;
-            }
+            if (!theme || !audio || !visuel) {
 
-            // Si des champs requis ne sont pas remplis, arrête la soumission du formulaire et affiche un message d'erreur
-            if (!isValid) {
                 errorElement.text('Veuillez remplir tous les champs.');
                 event.preventDefault(); // Empêche l'envoi du formulaire
             }
         });
-    }) */
+    })
 </script>
-
 
 <script>
     $(document).ready(function() {
