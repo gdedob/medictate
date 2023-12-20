54211cf71b9138e80ef334dca47a9c05b51bf368
Artisthur <148750933+Artisthur@users.noreply.github.com>
2023-12-20 21:23:15 +0100

    Page reçu directement en lien avec la page contact

---
+++ medictate/page-recu.php
@@ -0,0 +1,48 @@
+<?php /*Template Name: Reçu */ get_header(); ?>
+
+
+
+<br>
+<br>
+
+<br>
+
+<br>
+
+
+<br>
+<br>
+<div class="container">
+    <h1><?= $_POST['nom'] ?>, votre message s'envole vers nous! <br>
+    Nous y répondrons dès que possible</h1>
+
+    <br>
+    <br>
+
+   
+
+</div>
+
+
+
+
+
+
+
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+
+
+<!-- <?php var_dump($_POST); ?> -->
+
+ <?php get_footer(); ?>
+
+
